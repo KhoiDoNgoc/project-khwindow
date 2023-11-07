f61f1febe93403026a2de4d4b2e4015dc5ba6304
Ngoc Khoi <khoidng13@gmail.com>
2023-11-07 16:13:14 +0700

    Up

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DO NGOC KHOI
+          NGOC KHOI          
         </p>
+        <h2>Heloo React JS</h2>
       </header>
     </div>
   );
