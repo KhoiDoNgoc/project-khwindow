3a18e9136ea4ba00cc710e9b83105ac3963deece
Ngoc Khoi <khoidng13@gmail.com>
2023-11-09 10:03:25 +0700

    Upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
+//function App() {
+  //return (
+    //<div className="App">
+      //<header className="App-header">
+        //<img src={logo} className="App-logo" alt="logo" />
+        //<p>
+          //NGOC KHOI          
+        ////</p>
+        //<h2>Heloo React JS</h2>
+      //</header>
+    //</div>
+  //);
+//}
+
+//export default App;
+
+function ColorBox(prop){
+  const { color } = prop;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NGOC KHOI          
-        </p>
-        <h2>Heloo React JS</h2>
-      </header>
-    </div>
-  );
+     <div className="box" style={{backgroundColor:color}}></div>
+  )
 }
 
-export default App;
+export default ColorBox;
